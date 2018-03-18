3874fb51694437a9c062462a58887fcba7e9e0bc
win 8 pc <surajg097@gmail.com>
2018-03-18 20:01:14 +0530

    second

--- app/src/main/java/com/practise/dummy/MainActivity.java
+++ app/src/main/java/com/practise/dummy/MainActivity.java
@@ -2,6 +2,7 @@ package com.practise.dummy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,11 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("MainActivity","onCreate()");
     }
 
     public void FetchData(View view){
-        Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show();
-        vegSnacksData  vegSnacksData=new vegSnacksData();
-        vegSnacksData.getVegSnacksData(this);
+        Log.e("MainActivity","FetchData()");
+        //Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show();
+        /*vegSnacksData  vegSnacksData=new vegSnacksData();
+        vegSnacksData.getVegSnacksData(this);*/
+        TestHelper testHelper=new TestHelper(this);
+
     }
 }
